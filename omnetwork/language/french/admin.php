1b75fb6b13d5c407a197c194b6f2aa62c28986a5
mambax7 <mambax7@gmail.com>
2015-05-05 09:22:27 -0400

    Original files

---
+++ omnetwork/language/french/admin.php
@@ -0,0 +1,25 @@
+<?php 
+define('_AD_NAV_ACTIONS', "Actions");
+define('_AD_NAV_CPHOME', "Index Administration");
+define('_AD_NAV_SITEHOME', "Accueil Public");
+define('_AD_NAV_XOOPSORG', "News Xoops.org");
+define('_AD_NAV_LOGOUT', "D&eacute;connexion");
+define('_AD_NAV_PREFS', "Pr&eacute;f&eacute;rences");
+define('_AD_NAV_GENERAL', "Param&agrave;tres G&eacuten&eacute;raux");
+define('_AD_NAV_USERSETTINGS', "Param&agrave;tres des infos utilisateurs");
+define('_AD_NAV_METAFOOTER', "Meta Tags et Pied de page");
+define('_AD_NAV_CENSOR', "Options des mots censur&eacute;s");
+define('_AD_NAV_SEARCH', "Options de recherche");
+define('_AD_NAV_MAILER', "Param&agrave;tres Mail");
+define('_AD_NAV_MODULES', "Modules");
+define('_AD_NAV_MODADMIN', "Administration");
+define('_AD_NAV_BLOCKS', "Blocs");
+define('_AD_NAV_BLVIS', "Liste des blocs visibles");
+define('_AD_NAV_BLHID', "Liste des blocs cach&eacute;s");
+define('_AD_NAV_BLALL', "Lister tous les blocs");
+define('_AD_NAV_BLNEW', "Ajouter un nouveau bloc");
+define('_AD_NAV_USERS', "Utilisateurs");
+define('_AD_NAV_MISC', "Outils divers");
+
+define('_AD_NAV_MODHOME', "Voir le module");
+?>
---
+++ omnetwork/language/french/main.php
@@ -0,0 +1,8 @@
+<?php
+define("_MA_NAV_ACTIONS", "Menu Utilisateur");
+define("_MA_NAV_VIEWACCOUNT", "Voir son compte");
+define("_MA_NAV_EDITACCOUNT", "Editer son compte");
+define("_MA_NAV_NOTIFICATIONS", "Notifications");
+define("_MA_NAV_LOGOUT", "D&eacute;connexion");
+define("_MA_NAV_ADMINISTRATION", "Administration");
+?>
